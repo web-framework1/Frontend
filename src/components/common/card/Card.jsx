94c9249c02885330b7c78dbfa6d63f895151ba41
hs-2191214-parkjunseo <02justine@naver.com>
2025-10-31 11:51:43 +0900

    fix : 경로 수정

--- src/components/common/card/Card.jsx
+++ src/components/common/card/Card.jsx
@@ -1,1 +1,18 @@
-import React, { useMemo } from "react";
+import React from "react";
+import { cn } from "@utils/functions/utils";
+
+// 수정 필요
+function Card({ className, ...props }) {
+  return (
+    <div
+      data-slot="card"
+      className={cn(
+        "bg-card text-card-foreground flex flex-col gap-6 rounded-xl border",
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+export { Card };
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,22 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+
 import path from "path";
 import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
-import svgr from "vite-plugin-svgr";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react(), svgr(), tailwindcss()],
+  plugins: [tailwindcss()],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+    // 절대 경로를 위한 설정
+    alias: [
+      { find: "@", replacement: "/src" },
+      { find: "@assets", replacement: "/src/assets" },
+      { find: "@components", replacement: "/src/components" },
+      { find: "@pages", replacement: "/src/pages" },
+      { find: "@utils", replacement: "/src/utils" },
+    ],
   },
 });
